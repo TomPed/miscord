f5ce53d5bfb1c55e1eee896aac1a7900e5382f38
Tom Ped <tomped123456789@gmail.com>
2016-05-28 18:57:27 -0400

    Added case for gg

--- main.js
+++ main.js
@@ -3,19 +3,22 @@ var playFile = require('./lib/playFile');
 var Discord = require('discord.js');
 
 var bot = new Discord.Client();
-var availableCommands = ['%bag'];
+var availableCommands = ['.bag, .gg'];
 
 bot.on('message', function (msg) {
   var voiceChannel = msg.author.voiceChannel;
   var message = msg.content;
 
-  if (!message.startsWith('%')) {
+  if (!message.startsWith('.')) {
     return;
   }
 
   switch (message) {
-    case '%bag':
+    case '.bag':
       playFile(bot, voiceChannel, 'https://hydra-media.cursecdn.com/dota2.gamepedia.com/e/ee/Wdoc_inthebag_01.mp3');
+      break;
+    case '.gg':
+      playFile(bot, voiceChannel, 'audio/gg.mp3');
       break;
     default:
       bot.reply(msg, 'available commands are: ' + availableCommands);
