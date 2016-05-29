00517e29020e14656b623cbe5316bc0a6f2f702b
Tom Ped <tomped123456789@gmail.com>
2016-05-28 21:25:07 -0400

    Quick fix

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var randomRange = require('./lib/randomRange');
 var Discord = require('discord.js');
 
 var bot = new Discord.Client();
-var availableCommands = ['.bag, .gg'];
+var availableCommands = ['.bag', '.gg', '.random'];
 var random = ['audio/duh1.mp3', 'audio/duh2.mp3', 'audio/duh3.mp3'];
 var randomNumber = random.length;
 
@@ -17,17 +17,17 @@ bot.on('message', function (msg) {
   }
 
   switch (message) {
-    case '.bag':
+    case availableCommands[0]:
       playFile(bot, voiceChannel, '.125',  'https://hydra-media.cursecdn.com/dota2.gamepedia.com/e/ee/Wdoc_inthebag_01.mp3');
       break;
-    case '.gg':
+    case availableCommands[1]:
       playFile(bot, voiceChannel,'.25', 'audio/gg.mp3');
       break;
-    case '.random':
+    case availableCommands[2]:
       playFile(bot, voiceChannel, '1.2', random[randomRange(randomNumber)]);
       break;
     default:
-      bot.reply(msg, 'available commands are: ' + availableCommands);
+      bot.reply(msg, 'available commands are: ' + availableCommands.join(', '));
   }
 });
 
