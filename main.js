36aaec15ef94a2619194560f6e0115318422a359
Duncan Perkins <perkinsdt@goldmail.etsu.edu>
2016-06-05 21:00:22 -0400

    Game and User event logging

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 var parser = require('./lib/parser');
 var history = require('./lib/history');
+var presenceUpdated = require ('./lib/presence');
 var Discord = require('discord.js');
 
 var bot = new Discord.Client();
+var historyChannel = "";
 
 bot.on('voiceJoin', function (channel, user) {
   history(bot, channel, user);
+});
+
+bot.on('presence', function (oldUser, newUser) {
+  presenceUpdated(oldUser, newUser, bot);
 });
 
 bot.on('message', function (msg) {
