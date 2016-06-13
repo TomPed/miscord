7e9770edeb56573d2b28dab10a9d758a6eeb547b
Duncan Perkins <Duncan.Perkins@ClaytonHomes.com>
2016-06-13 15:34:14 -0400

    Generic collection object holding multiple class values example

--- main.js
+++ main.js
@@ -10,7 +10,15 @@ var db = new DbContext('preference.db');
 var bot = new Discord.Client();
 var commandCollection = require('./lib/Models/Commands/commandCollection');
 var audioCommand = require('./lib/Models/Commands/audioCommand');
+var randomAudioCommand = require('./lib/Models/Commands/randomAudioCommand');
 var commandMap = utils.getMap();
+
+var command = new audioCommand('.gg', 'audio/gg.mp3', '1', bot);
+var random = new randomAudioCommand('.random', 'audio/random/', '1', bot);
+var collection = new commandCollection();
+collection.Add(command);
+collection.Add(random);
+console.log(collection.length());
 
 db.getEntry({ _id: '123'}, function(docs) {
   console.log(docs);  
