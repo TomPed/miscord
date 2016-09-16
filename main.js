1ae4b694fb65c36000f14da8cfa5f40e267f41ce
Thomas Pedbereznak <tomped123456789@gmail.com>
2016-09-16 17:29:25 -0400

    test

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var eventLogging = require('./lib/eventLogging');
 var parser = require('./lib/parser');
 var Discord = require('discord.js');
 var utils = require('./lib/utils');
+var express = require('express');
+var app = express();
 
 var bot = new Discord.Client();
 var commandMap = utils.getMap();
@@ -26,3 +28,14 @@ bot.on('message', function (msg) {
 });
 
 bot.loginWithToken(process.env.BOT_TOKEN);
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
